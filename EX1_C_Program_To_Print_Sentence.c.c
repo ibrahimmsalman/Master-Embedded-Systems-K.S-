f3aa50fde72a3e40ff43f36cf2e9f1f7c7ac02af
ibrahimmsalman <115096040+ibrahimmsalman@users.noreply.github.com>
2022-10-10 23:59:20 +0200

    Unit_2_C_Programming

---
+++ EX1_C_Program_To_Print_Sentence.c.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : c.c
+ Author      : Ibrahim Salman
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+
+	printf("Hello World!\n");
+
+}
