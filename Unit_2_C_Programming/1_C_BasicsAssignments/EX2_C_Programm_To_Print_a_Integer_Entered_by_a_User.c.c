e88092e7e7e6bb9ab166624ae4c3bb3e5e3a3aef
ibrahimmsalman <115096040+ibrahimmsalman@users.noreply.github.com>
2022-10-11 00:32:12 +0200

    Add files via upload

---
+++ Unit_2_C_Programming/1_C_BasicsAssignments/EX2_C_Programm_To_Print_a_Integer_Entered_by_a_User.c.c
@@ -0,0 +1,25 @@
+/*
+ ============================================================================
+ Name        : main.c
+ Author      : Ibrahim Salman
+ Version     :
+ Copyright   : 
+ Description : C_Program_To_Print_Sentence.c, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+int main()
+{
+    int num;
+    fflush(stdin); fflush(stdout);
+    printf("Enter a integer: ");
+    scanf("%d",&num);
+    printf("You entered: %d",num);
+    return 0;
+}
+
+
+
+
+
