789f146a17bc20346fe04ceeafc7e3dca8c7d635
ibrahimmsalman <ia7586147@gmail.com>
2022-10-28 18:28:08 +0200

    editing in q2 and q7

--- Unit_2_C_Programming/3_C_FunctionsAssignment/Quiz/Q7_C_program_finds_and_print_the_last_occurrence_of_a_number_in_a_list_of_numbers_entered_in_an_array_by_the_user.c
+++ Unit_2_C_Programming/3_C_FunctionsAssignment/Quiz/Q7_C_program_finds_and_print_the_last_occurrence_of_a_number_in_a_list_of_numbers_entered_in_an_array_by_the_user.c
@@ -4,14 +4,39 @@
  Author      : Ibrahim Salman
  Version     :
  Copyright   : 
- Description : Hello World in C, Ansi-style
+ Description : C_program_finds_and_print_the_last_occurrence_of_a_number_in_a_list_of_numbers_entered_in_an_array_by_the_user, Ansi-style
  ============================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts(""); /* prints  */
-	return EXIT_SUCCESS;
+void find_lastindex(int arr[],int num);
+
+int main()
+{
+	int arr[10]={10,20,5,8,9,20,5,45,90,10};
+	int x;
+	printf("Enter your search number:\n");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&x);
+
+	find_lastindex(arr,x);
+
+}
+
+void find_lastindex (int arr[],int num)
+{
+	int i, k=-1;
+	for(i=0;i<10;i++)
+	{
+		if(num==arr[i])
+			k=i;
+		else
+			continue;
+
+	}
+
+	printf("\nThe index of %d is: %d", num, k);
+
 }
