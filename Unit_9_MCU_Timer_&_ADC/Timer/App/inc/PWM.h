6182ebc2a6cfba180040fefc2bdbb551163f4007
unknown <ia7586147@gmail.com>
2023-05-08 23:55:02 +0300

    finishing timer assignment

---
+++ Unit_9_MCU_Timer_&_ADC/Timer/App/inc/PWM.h
@@ -0,0 +1,13 @@
+/*
+ * PWM.h
+ *
+ *  Created on: May 8, 2023
+ *      Author: Ibrahim Salman
+ */
+
+#ifndef APP_INC_PWM_H_
+#define APP_INC_PWM_H_
+
+void PWM_Test(void);
+
+#endif /* APP_INC_PWM_H_ */
---
+++ Unit_9_MCU_Timer_&_ADC/Timer/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: May 8, 2023
+ *      Author: Ibrahim
+ */
+
+#include "atmega32_TIMER0_Test.h"
+
+int main(void)
+{
+   atmega32_TIMER0_Test();
+}
+
